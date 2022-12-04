8bdd9857d64d5158b6174a3c440ef398727e4f9b
Ruben <adityaprasad7123@gmail.com>
2022-12-03 23:23:22 -0400

    Selection Logic was made

---
+++ app.js
@@ -0,0 +1,50 @@
+// Gets Random Item
+function randomItem(items) {
+    return items[Math.floor(Math.random() * items.length)]
+}
+
+// Gets computer choice
+function getComputerChoice() {
+    const choices = ["ROCK", "PAPER", "SCISSORS"]
+    return randomItem(choices)
+}
+
+// Promt user for input
+function playerSelection() {
+    const input = prompt("Rock, Paper or Scissors")
+    return input.toUpperCase()
+}
+
+// Game Logic
+function playGame(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        console.log("Tie Game")
+    }
+    if (computerChoice === "ROCK" && playerChoice === "PAPER") {
+        console.log("You Win!")
+    }
+
+    if (computerChoice === "PAPER" && playerChoice === "SCISSORS") {
+        console.log("You Win!")
+    }
+    if (computerChoice === "SCISSORS" && playerChoice === "ROCK") {
+        console.log("You Win!")
+    }
+    if (playerChoice === "ROCK" && getComputerChoice === "PAPER") {
+        console.log("You Lose!")
+    }
+
+    if (playerChoice === "PAPER" && getComputerChoice === "SCISSORS") {
+        console.log("You Lose!")
+    }
+    if (playerChoice === "SCISSORS" && getComputerChoice === "ROCK") {
+        console.log("You Lose!")
+    }
+}
+
+const computerChoice = getComputerChoice()
+const playerChoice = playerSelection()
+
+
+console.log(computerChoice + ", " + playerChoice)
+playGame(computerChoice, playerChoice)
